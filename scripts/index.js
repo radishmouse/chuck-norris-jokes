c6df74f7e4c59fba4c40b276011ed2fa3e88d6bd
Chris Aquino <chris@radishmouse.com>
2019-12-10 18:16:19 -0500

    finishes small exercises

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,10 @@
 const jokeServerAddress = "https://api.chucknorris.io/jokes/random";
 
+// Contaier for our joke(s) and buttons.
+// We'll assign it later
+let jokeContainer;
+let jokeButton;
+
 function convertToJson(response) {
     console.log(response);
     return response.json();
@@ -11,19 +16,41 @@ function printJoke(joke) {
 
 function extractJoke(dataObject) {
     // debugger;
-    // return dataObject.value;
-    return "Knock knock. Who's there? Oakley."
+    // console.log(dataObject.value);
+    return dataObject.value;
+    // return "Knock knock. Who's there? Oakley."
 }
 
 function rendersJokeToPage(jokeString) {
     const h1 = document.createElement('h1');
     h1.textContent = jokeString;
-    document.body.appendChild(h1);
+    // document.body.appendChild(h1);
+    jokeContainer.textContent = '';
+    jokeContainer.appendChild(h1);
 }
 
-
-const whatIsFetch = fetch(jokeServerAddress)
-    // .then(r => r.json())       // Hipster version
+function fetchJoke() {
+    fetch(jokeServerAddress)
+    .then(r => r.json())       // Hipster version
     // .then(convertToJson)          // Named-function version
-    // .then(extractJoke)
-    // .then(rendersJokeToPage)
+    .then(extractJoke)
+    .then(rendersJokeToPage)
+}
+
+// const whatIsFetch = fetch(jokeServerAddress)
+function createJokeContainer() {
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+    return container;
+}
+
+function createJokeButton() {
+    const button = document.createElement('button');
+    button.textContent = 'Tell me a joke!';
+    document.body.appendChild(button);
+    return button;    
+}
+
+jokeContainer = createJokeContainer();
+jokeButton = createJokeButton();
+jokeButton.addEventListener('click', fetchJoke);
